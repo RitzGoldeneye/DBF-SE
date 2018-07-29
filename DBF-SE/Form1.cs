637e3f436acdf3b6eac70b2ec18b88063b37429f
Justin Rittmeyer <neoritz@hotmail.com>
2018-07-29 21:06:07 +1000

    reset

---
+++ DBF-SE/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBF_SE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                const string message = "Do you really want to exit?";
+                const string caption = "Exit";
+                var result = MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    Application.Exit();
+                }
+                else if (result != DialogResult.Yes)
+                {
+                    //Close dialog and do nothing
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            {
+                const string message = "Do you really want to exit?";
+                const string caption = "Exit";
+                var result = MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    Application.Exit();
+                }
+                else if (result != DialogResult.Yes)
+                {
+                    //Close dialog and do nothing
+                }
+            }
+        }
+    }
+}
